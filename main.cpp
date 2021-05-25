1def5f99c4471631384b2b259d181d2ec89af42d
HerrNilsen-K <typnilsen@gmail.com>
2021-05-25 15:13:13 +0200

    Created a demo

--- main.cpp
+++ main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include "SPT/string.hpp"
-#include <string>
 #include <algorithm>
+#include <random>
+#include <chrono>
 
 int main(int argc, char const *argv[]) {
+    //DEMO
     spt::string str("Hello ");
     str += spt::toString((int) 55);
     spt::string ss(" World");
     spt::string s2;
     s2 += str + ss + '!';
+    //Finding substrings
+    int pos;
+    try {
+        pos = (int)s2.find(str);
+        std::cout << "Substring found at position: " << pos << std::endl;
+    } catch (...) {
+        std::cout << "Substring not found" << std::endl;
+    }
+    //Transformation
     s2.toLower(s2.begin() + 2, s2.end());
     s2.replace(0, ss);
     std::cout << s2 + ' ' + "?" << std::endl;
     std::cout << s2 << std::endl;
     std::cout << std::endl;
-    int pos;
-    try {
-        pos = s2.find(str);
-        std::cout << "Found at position: " << pos << std::endl;
-    } catch (...) {
-        std::cout << "Substring not found" << std::endl;
-    }
 
+    //Capability with the stl
+    std::shuffle(s2.begin(), s2.end(), std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count()));
+    for(auto c : s2)
+        std::cout << c;
+    std::cout << std::endl;
+
+    //Mr. Hardys example
     spt::string A("Hallo"), B;
     A = "Guten Tag,";
     B = A + " wie gehts es?";
@@ -31,13 +42,6 @@ int main(int argc, char const *argv[]) {
     A = A + spt::toString(14);
     A += B;
     std::cout << A << std::endl;
-
-
-    spt::string mstr("Hello");
-    std::cout << mstr + 'e' + 'e';
-    std::cout << mstr;
-
-
 
     return 0;
 }
